176905def25ba8804af28ae573ad9691f8ce020c
Balint Soos <soosbalint95@gmail.com>
2020-05-29 00:08:50 +0200

    solve problem 1

---
+++ problems/1-two-sums/solution.js
@@ -0,0 +1,13 @@
+const twoSum = (nums, target) => {
+  const hashTable = {};
+  for (let i = 0; i < nums.length; i++) {
+    const hashKey = nums[i].toString();
+    if (hashTable.hasOwnProperty(hashKey)) {
+      return [hashTable[hashKey], i];
+    }
+    const newHashKey = (target - nums[i]).toString();
+    hashTable[newHashKey] = i
+  }
+};
+
+console.log(twoSum([2,7,11,15], 9))
