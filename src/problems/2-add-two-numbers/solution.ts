a9f631af98b083427b0fbdaf0a2751ffcc822af8
Balint Soos <soosbalint95@gmail.com>
2021-10-22 15:05:51 +0200

    optimize solution 2

--- src/problems/2-add-two-numbers/solution.ts
+++ src/problems/2-add-two-numbers/solution.ts
@@ -3,24 +3,21 @@ import { ListNode } from './list-node';
 export function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
   const head = new ListNode();
   let current = head;
-  let n1 = l1;
-  let n2 = l2;
-  let carry = 0;
-  while (n1 !== null || n2 !== null) {
-    let sum = carry;
-    if (n1 !== null) {
-      sum += n1.val;
-      n1 = n1.next;
+  let sum = 0;
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
     }
-    if (n2 !== null) {
-      sum += n2.val;
-      n2 = n2.next;
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
     }
     current.next = new ListNode(sum % 10);
     current = current.next;
-    carry = sum >= 10 ? 1 : 0;
+    sum = sum >= 10 ? 1 : 0;
   }
-  if (carry === 1) {
+  if (sum === 1) {
     current.next = new ListNode(1);
   }
   return head.next;
