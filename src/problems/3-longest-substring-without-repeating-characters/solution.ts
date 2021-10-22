157eca67157bab2884378b80c5c14cd562a07b4b
Balint Soos <soosbalint95@gmail.com>
2021-10-22 16:03:35 +0200

    optimize solution 3

--- src/problems/3-longest-substring-without-repeating-characters/solution.ts
+++ src/problems/3-longest-substring-without-repeating-characters/solution.ts
@@ -1,14 +1,13 @@
 export function lengthOfLongestSubstring(s: string): number {
+  const length = s.length;
   let max = 0;
   let current = '';
-  for (let i = 0; i < s.length; i++) {
+  for (let i = 0; i < length; i++) {
     if (current.includes(s[i])) {
       current = current.split(s[i])[1];
     }
     current = current.concat(s[i]);
-    if (current.length > max) {
-      max = current.length;
-    }
+    max = Math.max(max, current.length);
   }
   return max;
 }
