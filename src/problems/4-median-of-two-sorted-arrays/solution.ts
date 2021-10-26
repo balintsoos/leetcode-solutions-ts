ba52ded6f89e178b9c9f159764778d976aa0645e
Balint Soos <soosbalint95@gmail.com>
2021-10-26 02:06:58 +0200

    solve problem 4

--- src/problems/4-median-of-two-sorted-arrays/solution.ts
+++ src/problems/4-median-of-two-sorted-arrays/solution.ts
@@ -17,68 +17,38 @@ function medianOfOneArray(n: number[]): number {
 }
 
 function medianOfTwoArrays(n1: number[], n2: number[]): number {
-  if (isEven(n1.length + n2.length)) {
-    const middle = (n1.length + n2.length) / 2;
-    const l1 = n1.length;
-    const l2 = n2.length;
-    let i = 0;
-    let i1 = 0;
-    let i2 = 0;
-    let median = 0;
-    while (i < middle - 1) {
-      if (i > l1) {
-        median = n2[i2];
-        i2 += 1;
-        i += 1;
-        continue;
-      }
-      if (i > l2) {
-        median = n1[i1];
-        i1 += 1;
-        i += 1;
-        continue;
-      }
-      if (n1[i1] < n2[i2]) {
-        median = n1[i1];
-        i1 += 1;
-      } else {
-        median = n2[i2];
-        i2 += 1;
-      }
-      i += 1;
-    }
-    return median;
-  }
-  const middle = Math.trunc((n1.length + n2.length) / 2);
-  const l1 = n1.length;
-  const l2 = n2.length;
+  const length = n1.length + n2.length;
+  const n = [];
   let i = 0;
   let i1 = 0;
   let i2 = 0;
-  let median = 0;
-  while (i <= middle) {
-    if (i > l1) {
-      median = n2[i2];
+  while (i < length) {
+    if (i1 >= n1.length) {
+      n.push(n2[i2]);
       i2 += 1;
       i += 1;
       continue;
     }
-    if (i > l2) {
-      median = n1[i1];
+    if (i2 >= n2.length) {
+      n.push(n1[i1]);
       i1 += 1;
       i += 1;
       continue;
     }
     if (n1[i1] < n2[i2]) {
-      median = n1[i1];
+      n.push(n1[i1]);
       i1 += 1;
-    } else {
-      median = n2[i2];
-      i2 += 1;
+      i += 1;
+      continue;
     }
-    i += 1;
+    if (n1[i1] >= n2[i2]) {
+      n.push(n2[i2]);
+      i2 += 1;
+      i += 1;
+      continue;
+    }
   }
-  return median;
+  return medianOfOneArray(n);
 }
 
 function isEven(n: number): boolean {
