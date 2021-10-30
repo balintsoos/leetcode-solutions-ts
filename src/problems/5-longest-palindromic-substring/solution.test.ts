c5d9ff99337cd6ccd39d47bdd8191fdac7766880
Balint Soos <soosbalint95@gmail.com>
2021-10-30 15:31:26 +0200

    solve edge case of problem 5

--- src/problems/5-longest-palindromic-substring/solution.test.ts
+++ src/problems/5-longest-palindromic-substring/solution.test.ts
@@ -4,4 +4,8 @@ describe('5. Longest Palindromic Substring', () => {
   test('should return the character if input has only one character', () => {
     expect(longestPalindrome('a')).toEqual('a');
   });
+
+  test('should return the first character if input has two different characters', () => {
+    expect(longestPalindrome('ac')).toEqual('a');
+  });
 });
--- src/problems/5-longest-palindromic-substring/solution.ts
+++ src/problems/5-longest-palindromic-substring/solution.ts
@@ -1,3 +1,3 @@
 export function longestPalindrome(s: string): string {
-  return s;
+  return s[0];
 }
