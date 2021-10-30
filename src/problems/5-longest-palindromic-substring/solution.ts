37d331fe5c2f59b69f1379fc128d7cff469b196a
Balint Soos <soosbalint95@gmail.com>
2021-10-31 00:21:16 +0200

    optimize solution 5

--- src/problems/5-longest-palindromic-substring/solution.ts
+++ src/problems/5-longest-palindromic-substring/solution.ts
@@ -1,24 +1,24 @@
-type Tabulation = boolean | undefined;
+/*
+Example:
+Word: babad
+Tabulation Matrix:
+  0  1   2    3     4
+0 b ba bab baba babad
+1    a  ab  aba  abad
+2        b   ba   bad
+3             a    ad
+4                   d
+*/
 
 export function longestPalindrome(s: string): string {
-  const length = s.length;
-  if (length === 1) {
-    return s;
-  }
   let longest = s[0];
-  const tabulationMatrix: Tabulation[][] = [];
-  for (let i = 0; i < length; i++) {
-    const column: Tabulation[] = [];
-    column[i] = true;
-    tabulationMatrix.push(column);
-  }
-  // eslint-disable-next-line for-direction
-  for (let i = length - 2; i >= 0; i--) {
-    for (let j = i + 1; j < length; j++) {
-      if (equal(s[i], s[j]) && (adjacent(i, j) || tabulationMatrix[i + 1][j - 1] === true)) {
-        tabulationMatrix[i][j] = true;
-        const substring = s.substring(i, j + 1);
-        if (substring.length >= longest.length) {
+  const tabulationMatrix = getTabulationMatrix(s.length);
+  for (let column = 1; column <= s.length - 1; column++) {
+    for (let row = 0; row <= column - 1; row++) {
+      if (equal(s[column], s[row]) && (adjacent(column, row) || tabulationMatrix[column - 1][row + 1])) {
+        tabulationMatrix[column][row] = true;
+        const substring = s.substring(row, column + 1);
+        if (substring.length > longest.length) {
           longest = substring;
         }
       }
@@ -27,10 +27,21 @@ export function longestPalindrome(s: string): string {
   return longest;
 }
 
+function getTabulationMatrix(n: number): boolean[][] {
+  const matrix = [];
+  for (let i = 0; i < n; i++) {
+    const column = [];
+    column[i] = true;
+    matrix.push(column);
+  }
+  return matrix;
+}
+
 function equal(a: string, b: string): boolean {
   return a === b;
 }
 
 function adjacent(a: number, b: number): boolean {
-  return b - a === 1;
+  const diff = a - b;
+  return diff === 1 || diff === -1;
 }
