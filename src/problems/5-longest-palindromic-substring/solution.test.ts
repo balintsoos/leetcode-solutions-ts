2fa0f33e91aee4d3f66437d4f1b51977162de75f
Balint Soos <soosbalint95@gmail.com>
2021-10-30 23:24:12 +0200

    solve problem 5

--- src/problems/5-longest-palindromic-substring/solution.test.ts
+++ src/problems/5-longest-palindromic-substring/solution.test.ts
@@ -8,4 +8,17 @@ describe('5. Longest Palindromic Substring', () => {
   test('should return the first character if input has two different characters', () => {
     expect(longestPalindrome('ac')).toEqual('a');
   });
+
+  test('should return the substring if input has the same character multiple times next to each other', () => {
+    expect(longestPalindrome('cbbd')).toEqual('bb');
+  });
+
+  test('should return the longest palindrome', () => {
+    expect(longestPalindrome('abba')).toEqual('abba');
+    expect(longestPalindrome('babad')).toEqual('bab');
+  });
+
+  test('should handle long inputs', () => {
+    expect(longestPalindrome('babaddtattarrattatddetartrateedredividerb')).toEqual('ddtattarrattatdd');
+  });
 });
--- src/problems/5-longest-palindromic-substring/solution.ts
+++ src/problems/5-longest-palindromic-substring/solution.ts
@@ -1,3 +1,36 @@
+type Tabulation = boolean | undefined;
+
 export function longestPalindrome(s: string): string {
-  return s[0];
+  const length = s.length;
+  if (length === 1) {
+    return s;
+  }
+  let longest = s[0];
+  const tabulationMatrix: Tabulation[][] = [];
+  for (let i = 0; i < length; i++) {
+    const column: Tabulation[] = [];
+    column[i] = true;
+    tabulationMatrix.push(column);
+  }
+  // eslint-disable-next-line for-direction
+  for (let i = length - 2; i >= 0; i--) {
+    for (let j = i + 1; j < length; j++) {
+      if (equal(s[i], s[j]) && (adjacent(i, j) || tabulationMatrix[i + 1][j - 1] === true)) {
+        tabulationMatrix[i][j] = true;
+        const substring = s.substring(i, j + 1);
+        if (substring.length >= longest.length) {
+          longest = substring;
+        }
+      }
+    }
+  }
+  return longest;
+}
+
+function equal(a: string, b: string): boolean {
+  return a === b;
+}
+
+function adjacent(a: number, b: number): boolean {
+  return b - a === 1;
 }
