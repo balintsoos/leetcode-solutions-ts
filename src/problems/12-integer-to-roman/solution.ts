e723708757a6372690c051c9f5250e62427a5cfe
Balint Soos <soosbalint95@gmail.com>
2023-04-16 22:05:05 +0200

    improve on solution 12

--- src/problems/12-integer-to-roman/solution.ts
+++ src/problems/12-integer-to-roman/solution.ts
@@ -1,23 +1,8 @@
-const map = [
-  ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
-  ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
-  ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
-  ['', 'M', 'MM', 'MMM'],
-];
+const I = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+const X = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+const C = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+const M = ['', 'M', 'MM', 'MMM'];
 
 export function intToRoman(num: number): string {
-  let result = '';
-  let n = num;
-
-  result = map[0][n % 10] + result;
-  n = Math.floor(n / 10);
-
-  result = map[1][n % 10] + result;
-  n = Math.floor(n / 10);
-
-  result = map[2][n % 10] + result;
-  n = Math.floor(n / 10);
-
-  result = map[3][n] + result;
-  return result;
+  return M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
 }
